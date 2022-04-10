9ffe3973d5ba9cbf1ded9b423951c5c65327fa89
Sai Harini Voruganti <saiharinivoruganti@Sais-MacBook-Pro.local>
2022-04-10 18:41:40 -0500

    Itrn3

---
+++ BiologyAnimation/ClinicalTrials/IdentifyTheVariables/js/parameters.js
@@ -0,0 +1,31 @@
+var data = [
+	{
+		"id": 1,
+		"value": "Milk"
+	},
+	{
+		"id": 2,
+		"value": "Water"
+	},
+	{
+		"id": 3,
+		"value": "Applie Juice"
+	},
+	{
+		"id": 4,
+		"value": "Plant Height"
+	},
+	{
+		"id": 5,
+		"value": "Sunlight"
+	},
+	{
+		"id": 6,
+		"value": "Amount of Liquid"
+	},
+	{
+		"id": 7,
+		"value": "Soil"
+	}
+];	
+	
