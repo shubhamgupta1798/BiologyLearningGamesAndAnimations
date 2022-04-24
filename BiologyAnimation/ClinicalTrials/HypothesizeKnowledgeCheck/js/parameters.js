1f7518dd3de8a98c3e567f0bc0f1d7e785211173
shubhamgupta1798 <guptashubham1798@gmail.com>
2022-04-24 15:07:54 -0500

    Changes for HypothesizeKnowlegdeCheck

--- BiologyAnimation/ClinicalTrials/HypothesizeKnowledgeCheck/js/parameters.js
+++ BiologyAnimation/ClinicalTrials/HypothesizeKnowledgeCheck/js/parameters.js
@@ -1,15 +1,15 @@
 var data = [
 	{
 		"id": 1,
-		"value": "Six hours of sleep improves test scores.​"
+		"value": "Six hours of sleep improves test scores."
 	},
 	{
 		"id": 2,
-		"value": "Elementary aged children who eat dessert after lunch are more hyperactive than those who don’t.​"
+		"value": "Elementary aged children who eat dessert after lunch are more hyperactive than those who don't."
 	},
 	{
 		"id": 3,
-		"value": "Wounds should be covered with bandages to prevent scarring.​"
+		"value": "Wounds should be covered with bandages to prevent scarring."
 	},
 	{
 		"id": 4,
